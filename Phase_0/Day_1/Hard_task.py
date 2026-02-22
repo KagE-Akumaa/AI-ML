14d8d2797186c3d5a7d2dbac338c382757637bc2
KagE-Akumaa <mukuljogi18@gmail.com>
2026-02-22 21:22:28 +0530

    Hard task

--- Phase_0/Day_1/Hard_task.py
+++ Phase_0/Day_1/Hard_task.py
@@ -1,5 +1,6 @@
 # Task - Hard: You are given a raw list of IPs with duplicates: ["1.1.1.1", "8.8.8.8", "1.1.1.1", "10.0.0.5"]. Convert this to a set to deduplicate it, then create a dictionary that counts how many times each unique IP appeared in the original list.
 
+from collections import Counter
 
 ip_list = [
     "192.168.1.10",
@@ -16,8 +17,10 @@ ip_set = set(ip_list)
 print(ip_set)
 
 ip_dic = {x: 0 for x in ip_list}
+ip_dic2 = Counter(ip_list)
 
 for i in ip_list:
     ip_dic[i] += 1
 
 print(ip_dic)
+print(ip_dic2)
